f37aab5eb6c92ddd62bc7cac3aa947a99d240429
Jini42 <jiryoogal@googlemail.com>
2025-02-21 21:45:29 +0100

    .

--- RV_Interior/common/media/lua/server/BusDoubleDeckerServerDeal.lua
+++
@@ -1,3 +0,0 @@
-local vehicleName = "Base.DoubleDeckerBus"
-
-RVInterior.addInterior(vehicleName, { 24302, 12007, 0 }, {-1, 8, 0})
--- RV_Interior/common/media/lua/server/TrailerMovingServerDeal.lua
+++
@@ -1,4 +0,0 @@
-local vehicleName = "Base.Trailermovingbig"
-
-RVInterior.addInterior(vehicleName, { 23700, 12000, 0 }, {0, 14, 0})
-RVInterior.canEnterFromBack(vehicleName,true)
